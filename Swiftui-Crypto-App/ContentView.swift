d030db5e6eb587c8b6aa33120e3bbeb8628e1e04
Charizard17 <3723esad@gmail.com>
2023-08-25 23:40:24 +0200

    Initial Commit

---
+++ Swiftui-Crypto-App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Swiftui-Crypto-App
+//
+//  Created by Esad Dursun on 25.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftui-Crypto-App/Swiftui_Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_Crypto_AppApp.swift
+//  Swiftui-Crypto-App
+//
+//  Created by Esad Dursun on 25.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
