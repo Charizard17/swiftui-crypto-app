3c392d42d6ee5650bf1b3f5176a17a11cc02de3e
Charizard17 <3723esad@gmail.com>
2023-08-26 10:34:02 +0200

    ColorTheme created

---
+++ Swiftui-Crypto-App/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Swiftui-Crypto-App
+//
+//  Created by Esad Dursun on 25.08.23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
