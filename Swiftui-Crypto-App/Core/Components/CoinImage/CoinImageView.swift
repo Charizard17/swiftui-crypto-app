34e2c8b5893c7e0b0d4c7f13d99cbf1aafb9e4dc
Charizard17 <3723esad@gmail.com>
2023-09-09 20:42:05 +0200

    coin image view, service, model created. download coin images

---
+++ Swiftui-Crypto-App/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,40 @@
+//
+//  CoinImageView.swift
+//  Swiftui-Crypto-App
+//
+//  Created by Esad Dursun on 09.09.23.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
