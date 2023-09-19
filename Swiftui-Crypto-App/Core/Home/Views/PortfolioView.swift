a04d88397d42700fb08a4330b4009dfb2ac278b0
Charizard17 <3723esad@gmail.com>
2023-09-19 21:48:44 +0200

    missing assets folder added

---
+++ Swiftui-Crypto-App/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioView.swift
+//  Swiftui-Crypto-App
+//
+//  Created by Esad Dursun on 19.09.23.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text("Portfolio View")
+    }
+}
+
+struct PortfolioView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioView()
+    }
+}
